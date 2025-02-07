c8f59a5f232cd0264f6514bd21518f3123771ed1
akshayst369 <akshayst369@gmail.com>
2025-02-07 12:06:11 +0530

    Feat: Props 2nd method

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -2,9 +2,17 @@ import React from 'react'
 import Name from './components/Name'
 
 const App = () => {
+
+  const userData={
+    fname: "Akshay",
+    lname: "R",
+    ph_no: "1234567890"
+  }
   return (
     <div>
-      <Name name= "Akshay"/>
+      <Name fname ={userData.fname} 
+      lname = {userData.lname}
+       ph_no = {userData.ph_no} />
     </div>
   )
 }
--- front-end/src/components/Name.jsx
+++ front-end/src/components/Name.jsx
@@ -2,7 +2,9 @@ import React from "react";
 const Name = (props) => {
     return (
         <>
-            <h1>Name: {props.name}</h1>
+            <h1>Name: {props.fname}</h1>
+            <h1>Last Name : {props.lname}</h1>
+            <h1>Phone no: {props.ph_no}</h1>
         </>
     )
 };
